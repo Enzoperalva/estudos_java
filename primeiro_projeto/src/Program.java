66b47eb86c62db741e403d5981e8248b4d56331c
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-06-15 09:41:50 -0300

    Atividades java

--- primeiro_projeto/src/Program.java
+++ primeiro_projeto/src/Program.java
@@ -1,7 +1,21 @@
+import java.util.Locale;
 
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		int valor = 20;		
+		System.out.printf("Olá mundo!");
+		valor += 80 ;		
+		valor++;
+		
+		int valor2 = 190;
+		double valor4 = 12.2142;
+		System.out.print(valor);
+		System.out.println(valor2);
+		System.out.printf("%.2f%n", valor4);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.2f\n", valor4);
+		System.out.printf("%.5f\n", valor4);
 	}
 }
+
