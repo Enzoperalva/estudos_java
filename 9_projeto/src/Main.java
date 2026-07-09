1dd5db546a739909967073c0a0a3e16148f8a465
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-07-09 10:22:16 -0300

    Atividades logica com java

--- 9_projeto/src/Main.java
+++ 9_projeto/src/Main.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-	public void main(String[] args) {
-//		Scanner sc =
-		System.out.println("Ola mundo!");
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
 	}
 }
