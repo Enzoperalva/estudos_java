584cdbf9c0458cdca05282ce28b8f190deb9d899
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-07-12 17:26:39 -0300

    Desafios logica com java

--- 14_projeto/src/Main.java
+++ 14_projeto/src/Main.java
@@ -1,4 +1,13 @@
 
 public class Main {
-
+	public static void main(String[] args) {
+		int count = 0;
+		while(count < 100) {
+			System.out.println(++count);
+			if(count == 25) {
+				System.out.println("Chegou ao limite!");
+				break;
+			}
+		}
+	}
 }
