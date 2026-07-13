ec78472adfdaae4186a365f23ccecc2ea4819ce1
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-07-13 11:50:12 -0300

    Exercicios array java

--- 18_projeto/src/Main.java
+++ 18_projeto/src/Main.java
@@ -2,6 +2,39 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		int [] nums = {1, 2, 3, 4, 5, 6, 7};
+		int maiorSeq = 0, atualSeq = 0;
+		int i = 0;
+
+		for(int num: nums) {
+			i++;
+			if(i > nums.length) {
+				i--;
+			}
+			if(num < nums[i]) {
+				atualSeq = 0;
+			}
+			if(num > nums[i]) {
+				atualSeq++;
+			}
+			if(atualSeq > maiorSeq) {
+				maiorSeq = atualSeq;
+			}
+
+		}
+		System.out.println(maiorSeq);
+
+//		for(int i=0; i < tamArray ;i++) {
+//			if(nums[i++] < nums[++i]) {
+//				atualSeq = 0;
+//			}
+//			if(nums[i++] > nums[++i]) {
+//				atualSeq++;
+//				if(atualSeq > maiorSeq) {
+//					maiorSeq = atualSeq;
+//				}
+//			}
+//		}
 
 		input.close();
 	}
