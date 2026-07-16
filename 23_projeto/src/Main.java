d2fe99d8690dc419ef4d0882273c30086a24eb4a
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-07-16 11:55:29 -0300

    Metodos OO

---
+++ 23_projeto/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int res = multiplicaDoisNums(2, 2);
+		System.out.println(res);
+	}
+	public static int multiplicaDoisNums(int num1, int num2) {
+		return num1 * num2;
+	}
+}
